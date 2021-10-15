a7701d8aae17042538f35b5e7f3f273c0bf9b147
Sergey-PC <ShevchSMP70@ukr.net>
2021-10-15 20:18:04 +0300

    initial commit

---
+++ 001/Homework_001.py
@@ -0,0 +1,30 @@
+first = 10
+second = 30
+
+result = first + second     # сумма
+print('+ ', result)
+print(type(result))
+
+result = first - second     # разница
+print('- ', result)
+print(type(result))
+
+result = first * second     # умножение
+print('* ', result)
+print(type(result))
+
+result = first / second     # деление
+print('/ ', result)
+print(type(result))
+
+result = first ** second    # возведение в степень
+print('** ', result)
+print(type(result))
+
+result = first // second    # целочисленное деление
+print('// ', result)
+print(type(result))
+
+result = first % second     # остаток от деления
+print('% ', result)
+print(type(result))
