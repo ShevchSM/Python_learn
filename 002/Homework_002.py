8c38c14b9c040343691ec11e7dcf95a15fc11bb9
Sergey-PC <ShevchSMP70@ukr.net>
2021-10-19 14:37:21 +0300

    add 01-18

--- 002/Homework_002.py
+++ 002/Homework_002.py
@@ -1,1 +1,18 @@
-value = 12
+user_value_01 = input("Введи целое число:")
+user_value_01 = int(user_value_01)
+result_01 = user_value_01 * 2
+print(result_01, type(user_value_01))
+# print(int(input("Введи целое число:")) * 2)
+####################################################################################################
+user_value_02 = input("Введи число с точкой:")
+user_value_02 = float(user_value_02)
+result_02 = user_value_02 * 2
+print(result_02, type(user_value_02))
+# print(float(input("Введи число с точкой:")) * 2)
+####################################################################################################
+user_value_03 = input("Введи строку (текст):")
+user_value_03 = str(user_value_03)
+result_03 = user_value_03 * 2
+print(result_03, type(user_value_03))
+# print(str(input("Введи строку (текст):")) * 2)
+####################################################################################################
